9e8ee12f9617a2b01d48018db0ddcaba51c18364
Emil Wåreus <emil.wareus47@gmail.com>
2017-09-13 17:47:50 +0200

    Some more functionality in the threads

--- CV_AHR.py
+++ CV_AHR.py
@@ -5,11 +5,9 @@ Created on Tue Sep 12 08:20:49 2017
 @author: Emil Wåreus
 """
 
-from collections import deque
-import numpy as np
+
 import cv2
-import argparse
-import imutils
+
 import threading
 
 
@@ -18,111 +16,36 @@ class CV_AHR(threading.Thread):
     
     
     
-    def __init__(self, threadID, name):
+    def __init__(self, threadID, name, monitor):
         
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
-        
-        
-        self.ap = argparse.ArgumentParser()
-        self.ap.add_argument("-v", "--video",
-                             help="path to the (optional) video file")
-        self.ap.add_argument("-b", "--buffer", type=int, default=32,
-                             help="max buffer size")
-        self.args = vars(self.ap.parse_args())
-        
-        
-        
-        
-        self.greenLower = (29, 86, 6)
-        self.greenUpper = (64, 255, 255)
-         
-        self.pts = deque(maxlen=self.args["buffer"])
-        self.counter = 0
-        self.direction = ""
-        
-       
-        
-        
-    def get_frame(self, cap):
-        # Capture frame-by-frame
-        self.ret, self.frame = cap.read()
-        
+        self.monitor = monitor
     
-        self.frame = imutils.resize(self.frame, width=600)
-        self.blurred = cv2.GaussianBlur(self.frame, (11, 11), 0)
-        self.hsv = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
-        # Our operations on the frame come here
-        self.mask = cv2.inRange(self.hsv, self.greenLower, self.greenUpper)
-        self.mask = cv2.erode(self.mask, None, iterations=2)
-        self.mask = cv2.dilate(self.mask, None, iterations=2)
-         
-        self.cnts = cv2.findContours(self.mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-        
-        self.center = None
-        self.x, self.y = 0, 0
-        # only proceed if at least one contour was found
-    	 # only proceed if at least one contour was found
-        if len(self.cnts) > 0:
-    		 # find the largest contour in the mask, then use
-    		 # it to compute the minimum enclosing circle and
-    		 # centroid
-            self.c = max(self.cnts, key=cv2.contourArea)
-            ((self.x, self.y), self.radius) = cv2.minEnclosingCircle(self.c)
-            self.M = cv2.moments(self.c)
-            
-            self.center = (int(self.M["m10"] / self.M["m00"]), int(self.M["m01"] / self.M["m00"]))
-     
-    		 # only proceed if the radius meets a minimum size
-            if self.radius > 10:
-                 cv2.circle(self.frame, (int(self.x), int(self.y)), int(self.radius),(0, 255, 255), 2)
-                 cv2.circle(self.frame, self.center, 5, (0, 0, 255), -1)
-                 self.pts.appendleft(self.center) 
-       
-        
-        for i in np.arange(1, len(self.pts)):
-    
-            if self.pts[i - 1] is None or self.pts[i] is None:
-                continue
-    
-                 
-            self.thickness = int(np.sqrt(self.args["buffer"] / float(i + 1)) * 2.5)
-            cv2.line(self.frame, self.pts[i - 1], self.pts[i], (0, 0, 255), self.thickness)
-     
-        
-        
-        cv2.putText(self.frame, self.direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,0.85, (0, 0, 255), 3)
-        cv2.putText(self.frame, "X: {}, Y: {}".format(int(self.x), int(self.y)),
-                    (10, self.frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.35, (0, 0, 255), 1)
-     
-    	# show the frame to our screen and increment the frame counter
-
-        return self.frame, self.x, self.y
     
     def destroy(self):
+        print(self.name , "Destroyed")
         self.run=False
         
         
-
     def run(self):
         
         print("Starting " + self.name)
+        print("Press q to quit")
         self.run=True
+        self.monitor.init_CV(self)
         
-        cap = cv2.VideoCapture(1)
-        
-        
-        
+        cap = cv2.VideoCapture(1)  
+          
         while(self.run): 
-            
             
             key = cv2.waitKey(1) & 0xFF
             if key == ord("q"):
-                break
+                self.destroy()
+                
             if(cap.isOpened()):
-                frame, x, y = self.get_frame(cap)
+                frame, x, y = self.monitor.get_frame(self,cap=cap)
                 cv2.imshow("Frame", frame)
 
         
--- main.py
+++ main.py
@@ -5,18 +5,27 @@ Created on Wed Sep 13 11:38:23 2017
 @author: Emil Wåreus
 """
 
-from CV_AHR import CV_AHR
+
 
 from time import sleep
+from Monitor import Monitor
+from CV_AHR import CV_AHR
+
+
+
+    
+    
+
 
 if __name__ == "__main__":
     
-    CV_thread = CV_AHR(1, "Computer Vision Thread")
-	#monitor = Monitor(2, "Monitor Thread")
-	#monitor.start()
+    #Make Threads
+    CV_thread = CV_AHR(1, "Computer Vision Thread", monitor = Monitor)
+    
+        
+    #Start Threads
     CV_thread.start()
-    
-    sleep(20) #To destroy the thread
-    
-    CV_thread.destroy()
+        
+   
+
     
